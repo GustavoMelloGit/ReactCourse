78636d3a356466806442440029c7b98329427bd6
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2021-10-06 10:08:57 -0300

    feat: section 8 done

--- Section8/src/components/Form/index.jsx
+++ Section8/src/components/Form/index.jsx
@@ -1,17 +1,14 @@
 import React, { useState } from "react";
 import Button from "../Button";
 import Card from "../Card";
+import ErrorModal from "../ErrorModal";
 import Input from "../Input";
+import styles from "./Form.module.css";
 
-const defaultUsersData = [
-  {
-    name: "",
-    age: 0,
-  },
-];
 export default function Form(props) {
   const [name, setName] = useState("");
   const [age, setAge] = useState(0);
+  const [showModal, setShowModal] = useState(false);
 
   function handleChangeName(e) {
     setName(e.target.value);
@@ -19,32 +16,55 @@ export default function Form(props) {
   function handleChangeAge(e) {
     setAge(e.target.value);
   }
+  function resetInputs() {
+    setName("");
+    setAge(0);
+  }
+  function handleCloseModal() {
+    setShowModal(false);
+  }
+  function verifyInputUser() {
+    if (!name || !age) return false;
+    else return true;
+  }
   function handleAddUser(e) {
     e.preventDefault();
-    const userInputData = {
-      name: name,
-      age: age,
-      id: Math.random() * 10,
-    };
-    props.saveAddedUser(userInputData);
+    if (verifyInputUser()) {
+      const userInputData = {
+        name: name,
+        age: age,
+        id: Math.random() * 10,
+      };
+      props.saveAddedUser(userInputData);
+      resetInputs();
+    } else setShowModal(true);
   }
+
   return (
-    <Card>
-      <form onSubmit={handleAddUser}>
-        <Input
-          text="Username"
-          type="text"
-          onChange={handleChangeName}
-          value={name}
-        />
-        <Input
-          text="Age (Years)"
-          type="number"
-          onChange={handleChangeAge}
-          value={age}
-        />
-        <Button text="Add User" type="submit" />
-      </form>
-    </Card>
+    <div className={styles.container}>
+      <ErrorModal
+        title="An error occurred"
+        message="It looks like you just did something wrong"
+        showModal={showModal}
+        onClick={handleCloseModal}
+      />
+      <Card>
+        <form onSubmit={handleAddUser} className={styles.form}>
+          <Input
+            text="Username"
+            type="text"
+            onChange={handleChangeName}
+            value={name}
+          />
+          <Input
+            text="Age (Years)"
+            type="number"
+            onChange={handleChangeAge}
+            value={age}
+          />
+          <Button text="Add User" type="submit" />
+        </form>
+      </Card>
+    </div>
   );
 }
--- Section8/src/components/ListUser/index.jsx
+++ Section8/src/components/ListUser/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Card from "../Card";
 import User from "../User";
-import styles from "./ListUser.module.css";
 
 export default function ListUser(props) {
   return (
