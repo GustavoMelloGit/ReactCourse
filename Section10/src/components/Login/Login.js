d8feb84115eebddbe7bad49616e5a347fce6d6de
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2021-10-06 14:43:23 -0300

    feat: added context api to application

--- Section10/src/components/Login/Login.js
+++ Section10/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../context/auth";
 
 const emailReducer = (state, action) => {
   if (action.type === "USER_INPUT") {
@@ -24,6 +25,7 @@ const passwordReducer = (state, action) => {
 };
 
 const Login = (props) => {
+  const user = useContext(AuthContext);
   const [formIsValid, setFormIsValid] = useState(false);
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
     value: "",
@@ -62,7 +64,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    user.onLogin(emailState, passwordState);
   };
 
   return (
--- Section10/src/components/MainHeader/Navigation.js
+++ Section10/src/components/MainHeader/Navigation.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { useContext } from "react";
+import AuthContext from "../../context/auth";
 
-import classes from './Navigation.module.css';
+import classes from "./Navigation.module.css";
 
-const Navigation = (props) => {
+const Navigation = () => {
+  const user = useContext(AuthContext);
   return (
     <nav className={classes.nav}>
       <ul>
-        {props.isLoggedIn && (
+        {user.isLoggedIn && (
           <li>
             <a href="/">Users</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {user.isLoggedIn && (
           <li>
             <a href="/">Admin</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {user.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={user.onLogout}>Logout</button>
           </li>
         )}
       </ul>
