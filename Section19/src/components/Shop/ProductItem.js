0cd2ceb786c5c48653f8995be8c1f36f334444ff
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2021-10-13 15:00:44 -0300

    feat: class 256 done

--- Section19/src/components/Shop/ProductItem.js
+++ Section19/src/components/Shop/ProductItem.js
@@ -8,7 +8,12 @@ const ProductItem = (props) => {
   const { title, price, description, id } = props;
 
   const addToCartHandler = () => {
-    dispatch(cartActions.addItemToCart({ id, title, price, description }));
+    dispatch(cartActions.addItemToCart({
+      id,
+      title,
+      price,
+      description
+    }));
   }
   return (
     <li className={classes.item}>
--- Section19/src/store/cart-slice.js
+++ Section19/src/store/cart-slice.js
@@ -3,7 +3,6 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
     items: [],
     totalQuantity: 0,
-    totalAmount: 0
 }
 const cartSlice = createSlice({
     name: 'cart',
