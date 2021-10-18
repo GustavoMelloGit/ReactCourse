c4cb25d3472f28a71be5e4f94a83eaa5a805d425
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2021-10-18 14:49:58 -0300

    feat: section 24 done

---
+++ Section24/src/components/Modal/Modal.js
@@ -0,0 +1,33 @@
+import React from "react";
+import CSSTransition from "react-transition-group/CSStransition";
+
+import "./Modal.css";
+
+const animationTiming = {
+  enter: 400,
+  exit: 500,
+};
+
+const modal = (props) => {
+  return (
+    <CSSTransition
+      in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+      classNames={{
+        enterActive: "ModalOpen",
+        exitActive: "ModalClosed",
+      }}
+    >
+      <div className="Modal">
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
+  );
+};
+
+export default modal;
